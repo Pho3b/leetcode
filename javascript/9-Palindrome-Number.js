5161cc257d723ec1361dd6896cd11c2d0ffdc2bc
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-10-19 17:48:02 -0400

    Update 9-Palindrome-Number.js

--- javascript/9-Palindrome-Number.js
+++ javascript/9-Palindrome-Number.js
@@ -1,44 +1,73 @@
 /**
+ * Extra Space
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/palindrome-number/
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function (x) {
-    // Creates array from int characters
-    // 121 -> [1,2,1]
-    let arr = Array.from(String(x), Number);
+var isPalindrome = (x) => {
+    const arr = Array.from(String(x), Number);/* Space O(N) */
 
-    // Uses two pointer
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== arr[arr.length - 1 - i]) {
-            return false;
-        }
+    for (let i = 0; i < arr.length; i++) {    /* Time O(N) */
+        const [ left, right ] = [ arr[i], (arr[((arr.length - 1) - i)]) ];
+        
+        const isEqual = (left === right);
+        if (!isEqual) return false;
     }
 
     return true;
 };
 
-// Runtime: 302 ms, faster than 40.50% of JavaScript online submissions for Palindrome Number.
-// Memory Usage: 51.8 MB, less than 8.36% of JavaScript online submissions for Palindrome Number.
-
 /**
  * Reverse Integer Using Modulo
- * Time O(N) | Space O(1)
+ * Time O(log10(N)) | Space O(1)
  * https://leetcode.com/problems/palindrome-number/
  * @param {number} x
  * @return {boolean}
  */
- var isPalindrome = function(x) {
-    if (x < 0) return false;
+var isPalindrome = (x) => {
+    const isEdgeCase1 = (x === 0);
+    if (isEdgeCase1) return true;
+    
+    const isNeagtive = (x < 0);
+    const isTen = ((x % 10) === 0);
+    
+    const isEdgeCase2 = (isNeagtive || isTen);
+    if (isEdgeCase2) return false;
 
-    const inputX = x;
-    let revX = 0;
+    return search(x);/* Time O(log10(N)) */
+};
 
-    while (x > 0) {
-        revX += x % 10;
+var search = (x, previous = x, temp = 0) => {
+    while (temp < x) {/* Time O(log10(N)) */
+        const remainder = (x % 10);
+        
+        previous = x;
+        x = Math.floor(x / 10);
+        temp = (temp * 10) + remainder;
+    }
+    
+    return (x === temp || previous === temp);
+}
+
+/**
+ * Reverse Integer Using Modulo
+ * Time O(log10(N)) | Space O(1)
+ * https://leetcode.com/problems/palindrome-number/
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = (x, inputX = x, revX = 0) => {
+    const isEdgeCase = (x < 0);
+    if (isEdgeCase) return false;
+
+    const isPositive = () => (0 < x);
+    while (isPositive()) {/* Time O(log10(N)) */
+        revX += (x % 10);
         x = Math.floor(x / 10);
 
-        if (x > 0) revX *= 10
+        if (isPositive()) revX *= 10
     }
 
-    return revX === inputX;
+    return (revX === inputX);
 };
